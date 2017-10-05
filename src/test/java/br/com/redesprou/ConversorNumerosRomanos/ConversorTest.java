3b2fa93cb5972bd2ab4d828e774ae84324c829cd
asdf <asdf@gmail.com>
2017-10-04 21:19:27 -0300

    Adiciona Projeto

---
+++ src/test/java/br/com/redesprou/ConversorNumerosRomanos/ConversorTest.java
@@ -0,0 +1,26 @@
+package br.com.redesprou.ConversorNumerosRomanos;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ConversorTest extends TestCase {
+
+	@Test
+	public void test() {
+Conversor conversor = new Conversor();
+		
+		assertEquals(new Integer(1), conversor.converter("i"));
+		assertEquals(new Integer(9), conversor.converter("ix"));
+		assertEquals(new Integer(30), conversor.converter("xxx"));
+		assertEquals(null, conversor.converter("xxxx"));
+		assertEquals(null, conversor.converter("0"));
+		assertEquals(null, conversor.converter("ABC"));
+		assertEquals(null, conversor.converter(null));
+		assertEquals(null, conversor.converter("xiiii"));
+		assertEquals(new Integer(1280), conversor.converter("MCCLXXX"));
+		assertEquals(null, conversor.converter(""));
+		assertEquals(new Integer(4974), conversor.converter("MMMMCMLXXIV"));
+		
+	}
+}
